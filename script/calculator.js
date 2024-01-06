1137f29a61706aefcee74321e86dc5b67de655a1
FerBastias95 <119711413+FerBastias95@users.noreply.github.com>
2024-01-06 01:50:14 -0300

    Update calculator.js

--- script/calculator.js
+++ script/calculator.js
@@ -31,3 +31,52 @@ function addToDisplay(value) {
     var display = document.getElementById('display');
     display.value = value;
 }
+
+function calculateResult() {
+    var inputBoxes = document.getElementById('inputBoxes');
+    var numbers = [];
+    var output = document.getElementById('output');
+    for (var i = 0; i < inputBoxes.childElementCount; i++) {
+        var value = inputBoxes.children[i].value;
+        if (!isNaN(parseFloat(value))) {
+            numbers.push(parseFloat(value));
+        }
+    }
+    var display = document.getElementById('display');
+    var operator = display.value;
+    var result = numbers[0];
+
+    switch (operator) {
+        case '+':
+            for (var i = 1; i < numbers.length; i++) {
+                result += numbers[i];
+            }
+            break;
+        case '-':
+            for (var i = 1; i < numbers.length; i++) {
+                result -= numbers[i];
+            }
+            break;
+        case '*':
+            for (var i = 1; i < numbers.length; i++) {
+                result *= numbers[i];
+            }
+            break;
+        case '/':
+            for (var i = 1; i < numbers.length; i++) {
+                result /= numbers[i];
+            }
+            break;
+        case 'Promedio':
+            for (var i = 1; i < numbers.length; i++) {
+                result += numbers[i];
+            }
+            result = result / numbers.length;
+            break;
+
+        default:
+            result = '?';
+            break;
+    }
+    output.value = result;
+}
